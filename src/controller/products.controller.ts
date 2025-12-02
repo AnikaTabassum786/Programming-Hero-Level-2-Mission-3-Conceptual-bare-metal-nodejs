554ca7b82ab731c055c7e0233467ef86dc4d25f3
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-12-02 14:12:55 +0600

    get by id

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -1,15 +1,31 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../services/productService";
+import { IProduct } from "../types/productInterface";
 
 export const productController = (req: IncomingMessage, res:ServerResponse)=>{
 
    const url = req.url;
    const method = req.method;
+
+   const urlPart = url?.split('/')
+   const id = urlPart && urlPart[1] === 'products' ? Number(urlPart[2]):null
+   console.log(id)
    
    if(method === "GET" && url === '/products'){
        const products = readProduct()
        res.writeHead(200,{'content-type':'application/json'});
        res.end(JSON.stringify({message: 'This is Product route', data:products}))
    }
+   else if(method === "GET" && id!== null){
+    const products = readProduct();
+    const product = products.find((p:IProduct)=>p.id === id)
+    res.writeHead(200,{'content-type':'application/json'});
+    res.end(JSON.stringify({message: 'This is Product route', data:product}))
+   }
+   else if(method === 'POST' && url === '/products'){
+        const body =''
+   }
 
 }
+
+
